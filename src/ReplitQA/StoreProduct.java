6888f959a4fb8f4087f2cc4d2ac27ffa4cdf255d
Baris312 <barmagan81@gmail.com>
2021-01-03 22:52:58 -0600

    S

--- src/ReplitQA/StoreProduct.java
+++ src/ReplitQA/StoreProduct.java
@@ -4,70 +4,61 @@ package ReplitQA;
 public class StoreProduct {
 
 
+    String label;
+    int price;
+    String category;
+    boolean hasExpiration;
+    int stock;
 
-   public String label;
-   public int price;
-   public String category;
-   public boolean hasExpiration;
-   public int stock ;
+    //1.  A secondary constructor that does not have
+    //a category, expiration, or stock (stock defaults to 0)
+    public StoreProduct(String label, int price) {
+        this.label = label;
+        this.price = price;
+        this.stock = 0;
+        category = "misc";
+        hasExpiration = false;
+    }
 
-
-    //  * The main constructor
-    public StoreProduct(String label, int price, String category, boolean hasExpiration, int stock)
-    {
-        this.label = label;
-        this.price = price;
-        this.category = "misc";
-        this.hasExpiration = false;
-        this.stock = stock;
-    }
-//  * A secondary constructor that does not have
-//  a category or an expiration (category defaults to "misc", hasExpiration defaults to false)
-    public StoreProduct(String label, int price, int stock)
-    {
-        this.label = label;
-        this.price = price;
-        this.stock = stock;
-        category="misc";
-        hasExpiration=false;
-    }
-// * A secondary constructor that does not have a category, expiration, or stock (stock defaults to 0)
-    public StoreProduct(String label, int price)
-    {
-        this.label = label;
-        this.price = price;
-        stock= 0;
-        category="misc";
-        hasExpiration=false;
-    }
-//  * A secondary constructor that just does not have stock
-    public StoreProduct(String label, int price, String category, boolean hasExpiration)
-    {
-        this.label = label;
-        this.price = price;
+    //2.  A secondary constructor that just does not have stock
+    public StoreProduct(String label, int price, String category, boolean hasExpiration) {
+        this(label, price);
         this.category = category;
         this.hasExpiration = hasExpiration;
-
+        this.stock = 0;
     }
 
-    public void expired(boolean hasExpired)
-    {
-      if  (hasExpiration && hasExpired ) stock = 0 ;
+    //3. A secondary constructor that does not have a category or an expiration
+    // (category defaults to "misc", hasExpiration defaults to false)
+    public StoreProduct(String label, int price, int stock) {
+        this(label, price);
+        this.stock = stock;
+        category = "misc";
+        hasExpiration = false;
+    }
+
+    //4. the main Constructor: has everything
+    public StoreProduct(String label, int price, String category, boolean hasExpiration, int stock) {
+        this(label, price, stock);
+        this.category = category;
+        this.hasExpiration = hasExpiration;
     }
 
 
-
-    public boolean sale(int quantity )
-    {
-      if (quantity<= stock )   stock-= quantity;
-
-     return (quantity< stock ) ;
+    public void expired(boolean hasExpired) {
+        if (hasExpired) {
+            stock = 0;
+        }
     }
 
-     public double getDiscountedPrice(double discount)
-     {
-         return price -= discount * price;
-     }
+    public boolean sale(int quantity) {
+        if (quantity <= stock) stock -= quantity;
 
+        return (quantity < stock);
+    }
+
+    public double getDiscountedPrice(double discount) {
+        return (1 - discount) * price;
+    }
 
 }
