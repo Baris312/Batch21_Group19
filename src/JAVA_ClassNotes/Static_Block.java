c474491384872034afb246b3d2bdbcf578792a95
Baris312 <barmagan81@gmail.com>
2021-01-06 09:17:27 -0600

    Exceptions

---
+++ src/JAVA_ClassNotes/Static_Block.java
@@ -0,0 +1,40 @@
+package JAVA_ClassNotes;
+
+public class Static_Block {
+  static int v;
+   static String b;
+    static {
+        v=5;
+        b ="final";
+        System.out.println("you dont need main method to run this ");
+        System.exit(0); // this will stop main to print anything else or other methods
+    }
+
+    public static void main(String[] args) {
+      System.out.println("test");
+
+    }
+
+}
+
+
+/*   Static Block ; It is a static initializer.
+ It's executed when the class is loaded and a good place to put initialization of static variables.
+
+Benefits of static blocks ;
+
+1 * Static block can be used to show that a program can run without main function also. see example @  top .
+
+2 * It's a block of code which is executed when the class gets loaded by a classloader.
+It is meant to do initialization of static members of the class
+
+3 * Static blocks has priority  it will run first static class and block than main and others .
+
+
+
+
+
+
+
+
+ */
--- src/SoftskillProjects_ClassNOTES/SoftSkillQuestionnaire.java
+++ src/SoftskillProjects_ClassNOTES/SoftSkillQuestionnaire.java
@@ -4,12 +4,12 @@ public class SoftSkillQuestionnaire {
 }
 /*
 
-  ** Bug Ticket ;
+ 1-  ** Bug Ticket ;
     * Who to assign a bug ticket/report ?
         - 1.  to a developer
-        - 2.  tech lead / teamlead  (dev team = developers + testers)
+        - 2.  tech lead / team-lead  (dev team = developers + testers)
         - 3.  BA / PO
-
+ Q **Difference between bug severity and bug priority ?
 
      * bug priority ?
                  -> Severity level : how much does a bug effect to the whole project? (decided by testers )
@@ -18,13 +18,63 @@ public class SoftSkillQuestionnaire {
        - low level : less effective .
 
 
+2 -  what is  Backlog  ??
+     == Product backlog-  list of user story , bugs, task
+
+
+3 - What is blackbox Testing
+- Testing withOut Seeing actual code , generally  use in UAT testing .
+
+Q4 -  What reports do you use in Jira ?
+
+
+Q5 - Do you track the root of the issues in Jira  ? If yes How ?
+
+Q6 - How Do I know if test Case is good ?
+
+Q7 - What is the most complex test Case  you wrote ?
+
+Q8 - How to test the module of transferring money; over $10K  there is additional confirmation,
+ below $10K no confirmation. (check the boundary)
 
 
 
+Q9 - How do you calculate points for stories?
+
+
+Q10 - Suppose in a grooming meeting your developer gave a point
+ but you think you cannot finish it in that time. What do you do?
 
 
 
+Q11 - What is verification / validation?
 
+Q12 - What test cases would you not automate?
+
+Q13 - What percent of your test cases were automated?
+
+
+Q14 - What is Epic?
+
+Q15 - Why/when/where/how often do you run Regression?
+
+Q16 -Whats test strategy and test plan. What's difference
+
+Q17 - Tell me about testing pyramids(hierarchy/level) and their differences.
+
+Q18 - Testing process from scratch == STLC
+
+Q19 - What was your definition of ready and definition of done in your team?
+
+Q20 - Have you done performance testing?
+
+Q21 - Explain SDLC and STLC?
+
+
+Q22 -What is the difference between retesting and regression testing?
+
+
+Q23 - Can you also explain your previous project> what was it about? What did you do?
 
 
 
