2e56fc2bdf208b7c4bdcd53c4fd8323c3930bd04
Baris312 <barmagan81@gmail.com>
2021-01-12 01:38:18 -0600

    selenium

---
+++ src/JAVA_InterviewQuestions/Solution/Array_Max_Number.java
@@ -0,0 +1,45 @@
+package JAVA_InterviewQuestions.Solution;
+
+import java.util.Arrays;
+
+public class Array_Max_Number {
+    public static void main(String[] args) {
+
+
+        int[] num = {1, 20, 50, 4, 7, 12};
+        System.out.println(max(num));
+
+    }
+/*
+    public static int max(int [] num){
+        int max=0;  // must initialize  max as min as possible , we will reassign if we get bigger number
+
+        for (int each: num )
+        {
+            if (each>max)  max=each;
+        }
+        return max;
+    }
+
+
+ */
+  //   2-
+    public static int max(int [] num) {
+       int max= Integer.MIN_VALUE;
+        for (int each : num ) {
+            max = Math.max(each, max);
+        }
+
+            return max ;
+
+    }
+/* 3-
+            public static int max(int [] num){
+                Arrays.sort(num);
+                return num[num.length-1];
+            }
+
+
+
+ */
+}
---
+++ src/JAVA_InterviewQuestions/Solution/Array_Min.java
@@ -0,0 +1,48 @@
+package JAVA_InterviewQuestions.Solution;
+
+import java.util.Arrays;
+
+public class Array_Min {
+    public static void main(String[] args) {
+
+
+        int[] num = {1, 20, 50, 4, 7, 12};
+        System.out.println(min(num));
+
+    }
+    /*
+        public static int min(int [] num){
+            int min=5000;  // must initialize  min as max as possible , we will reassign if we get lower number
+
+            for (int each: num )
+            {
+                if (each<min)  min=each;
+            }
+            return min;
+        }
+
+
+     */
+    //   2-
+    public static int min(int [] num) {
+        int min= Integer.MAX_VALUE;
+        for (int each : num ) {
+            min = Math.min(each, min);
+        }
+
+        return min ;
+
+    }
+/* 3-
+            public static int min(int [] num){
+                Arrays.sort(num);
+                return num[0];
+            }
+
+
+
+
+            */
+}
+
+
