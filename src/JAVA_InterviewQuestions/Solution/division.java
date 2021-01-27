8bd0076a2138612d8ac1e96ab3c8afc9644840d3
Baris312 <barmagan81@gmail.com>
2021-01-26 20:22:31 -0600

    divisiable

---
+++ src/JAVA_InterviewQuestions/Solution/division.java
@@ -0,0 +1,58 @@
+package JAVA_InterviewQuestions.Solution;
+
+public class division
+    {
+        public static void main(String[] args)
+            {
+
+
+                divisiable();
+            }
+
+     public static void divisiable(){
+        String divisibleBy15 ="";
+
+                String divisibleBy5 ="";
+
+                String divisibleBy3="";
+
+
+
+                for(int i=1; i <= 100; i++)
+                    {
+
+
+                        if (i % 15 == 0 && i % 3 == 0)
+
+                            {
+                                divisibleBy15 += i + " ";
+                            }
+
+                        else if (i % 5 == 0 && i % 15 != 0)
+
+                            {
+                                divisibleBy5 += i + " ";
+                            }
+
+                        else if (i % 3 == 0 && i % 15 != 0)
+
+                            {
+                                divisibleBy3 += i + " ";
+                            }
+
+
+                    }
+                System.out.println("Divisible By 15: "+divisibleBy15);
+
+                System.out.println("Divisible By 5: "+divisibleBy5);
+
+                System.out.println("Divisible By 3: "+divisibleBy3);
+
+            }
+
+
+    }
+
+
+
+
